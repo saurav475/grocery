60f83ccac65b6bb39cab8340be89d9806f4f6f4d
Sourav Choudhury <sauravchoudhury541@gmail.com>
2024-12-05 00:31:29 +0530

    grocery app finished product

---
+++ grocery/admin/src/components/admin/footer/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="py-4 bg-footer mt-auto">
+                <div className="container-fluid">
+                    <div className="d-flex align-items-center justify-content-between small">
+                        <div className="text-muted-1">© 2020 <b>Gambo Supermarket</b>. by <a href="">Super Market</a></div>
+                        <div className="footer-links">
+                            <a href="">Privacy Policy</a>
+                            <a href="">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
